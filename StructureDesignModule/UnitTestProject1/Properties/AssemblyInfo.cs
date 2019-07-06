7f0997d1de43f9c6374812f9e5cd2b6a2132c816
doubility-fzm <464112498@qq.com>
2019-07-06 20:32:53 +0800

    design structure module completed without pavement

---
+++ StructureDesignModule/UnitTestProject1/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestProject1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestProject1")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("f0f1a3b0-d985-4a94-b085-c23b3bbddafa")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ StructureDesignModule/UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using StructureDesignModule;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var t1 = new Technical_Demand();
+            t1.Write_Info2TXT();
+            var t2 = new CrossSection();
+            t2.calculate_girder_parament(ref t1);
+            t2.Write_Info2TXT();
+            var t3 = new longitudinal_lateral_connection();
+            t3.get_parameters(ref t1, ref t3, ref t2);
+            t3.Write_Info2TXT();
+        }
+    }
+}
